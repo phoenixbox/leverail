fe6c842e75a759f20c9ef6803e4b67868ad4f649
phoenixbox <shane@quickleft.com>
2014-01-25 22:17:30 -0700

    app initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Leveredge::Application.config.secret_key_base = '8a8f6e1b0e19658d4c5ab70656a25fb4064341aca2343a10b7b1df1353f26b84b1ab92812c66739b87253dd68000ef37a9ff0cb156109d1deeb9ae0834a50825'
