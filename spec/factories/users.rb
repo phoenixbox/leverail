4d4a527738ff9334ef484da45591d782e1577c89
phoenixbox <shane@quickleft.com>
2014-01-26 16:05:31 -0700

    test fixed

--- spec/factories/users.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    name  { Faker::Name.name }
-    email { Faker::Internet.safe_email }
-    password 'password'
-    password_confirmation 'password'
-  end
-end
