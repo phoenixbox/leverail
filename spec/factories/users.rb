28c610fccb0cb9886ea2d2b95a4ac304f35a5ddc
phoenixbox <shane@quickleft.com>
2014-02-01 18:24:44 -0700

    removed old factories

--- spec/factories/users.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :user, :class => User do
-    sequence(:email) { |n| "#{n}#{Faker::Internet.email}" }
-    password 'password'
-    password_confirmation 'password'
-  end
-end
