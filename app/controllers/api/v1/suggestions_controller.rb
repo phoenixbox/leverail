742c6777d2d412fd28b60fad7fc59623c0b8f799
Shane Rogers <rogerssh@tcd.ie>
2014-10-08 23:55:23 -0700

    Update RABL JSON responses and create action redirects

--- app/controllers/api/v1/suggestions_controller.rb
+++ app/controllers/api/v1/suggestions_controller.rb
@@ -10,7 +10,9 @@ module Api
       def create
         suggestion = Suggestion.create(suggestion_params)
         if suggestion
-          redirect_to api_v1_suggestion_path(suggestion)
+          # TODO: Whats the best way to redirect with authentication
+          redirect_url = [api_v1_suggestion_path(suggestion),'?',@user.attributes.slice("email", "authentication_token").to_query].join
+          redirect_to redirect_url
         else
           # TODO: Retrieve parameter errors and return to the user
           render :json=> {:success=>false, :message=>"There was an error with your suggestion", :status=>401}, :status=>401
@@ -25,7 +27,8 @@ module Api
 
       def suggestion_params
         convert_lat_long_to_bigdecimal
-        params.require(:suggestion).permit(:address, :latitude, :longitude, :state, :city, :zip_code, :country, :canvas_type, :image_url)
+        params[:suggestion][:suggestor_id] = @user.id
+        params.require(:suggestion).permit(:address, :latitude, :longitude, :state, :city, :zip_code, :country, :canvas_type, :image_url, :suggestor_id)
       end
 
       def convert_lat_long_to_bigdecimal
--- app/views/api/v1/suggestions/index.json.rabl
+++ app/views/api/v1/suggestions/index.json.rabl
@@ -1,3 +1,7 @@
 collection @suggestions => :suggestions
 
 attributes :id, :address, :latitude, :longitude, :state, :city, :zip_code, :country, :canvas_type, :image_url
+
+child @user => :suggestor do
+  attributes :id, :email, :profile_image_url
+end
